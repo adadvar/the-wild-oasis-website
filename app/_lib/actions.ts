9fc52d0499dfc609df540260d3072671d42eb1e0
adadvar <a.dadvar_71@yahoo.com>
2024-10-03 13:07:56 +0330

    update profile with server action

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -1,6 +1,32 @@
 'use server';
 
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+
+export async function updateGuest(formData: FormData) {
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in')
+
+  const nationalID = formData.get('nationalID');
+  const nationalityEntry = formData.get('nationality') as string;
+
+  const [nationality, countryFlag] = nationalityEntry.split('%');
+
+  if (typeof nationalID !== 'string') throw new Error('National ID must be a string');
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) throw new Error('Please provide a valid national ID')
+
+  const updateData = { nationality, countryFlag, nationalID }
+
+  const { data, error } = await supabase
+    .from('guests')
+    .update(updateData)
+    //@ts-ignore
+    .eq('id', session?.user?.guestId)
+
+  if (error) throw new Error('Guest could not be updated');
+
+}
 
 export async function signInAction() {
   await signIn('google', { redirectTo: '/account' })
--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { createGuest, getGuest } from "./data-service"
 
 const authConfig = {
   providers: [
@@ -12,11 +13,29 @@ const authConfig = {
     //@ts-ignore
     authorized({ auth, request }) {
       return !!auth?.user
+    },
+    //@ts-ignore
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest) await createGuest({ email: user.email, fullName: user.name })
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    //@ts-ignore
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     }
   },
   pages: {
     signIn: '/login'
-  }
-}
+  },
+};
 
+//@ts-ignore
 export const { auth, signIn, signOut, handlers: { GET, POST } } = NextAuth(authConfig)
